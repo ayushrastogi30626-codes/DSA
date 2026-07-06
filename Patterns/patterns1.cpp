5109db6a98581516cf409691760f8be94c2dccd2
Ayush Rastogi <YOUR_NOREPLY_EMAIL>
2026-07-06 16:18:27 +0530

    1st Pattern problem

---
+++ Patterns/patterns1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n =5 ;
+    int i=1;
+         for(i =1 ; i<=n ; i++){
+    int j=1;
+        for(j=1; j<=n ; j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+        }
+    }
