f73cb84715f271ee5183c0454ff4ab64e0b8ebc1
Ayush Rastogi <YOUR_NOREPLY_EMAIL>
2026-07-12 17:58:07 +0530

    Update Pattern 6 code

--- Patterns/pattern6.cpp
+++ Patterns/pattern6.cpp
@@ -2,16 +2,19 @@
 using namespace std ;
 int main(){
     int row,col;
-    for(row=1 ; row<=5 ; row= row+1)
+    int n;
+    cout<<"Input he value: ";
+    cin>>n;
+    for(row=1 ; row<=n ; row= row+1)
     {
-        for(col=1 ; col<= 5-row ; col=col+1)
+        for(col=1 ; col<= n-row ; col=col+1)
         {
             cout<<"  ";
         }
         {
             for(col=1 ; col<= 2*row-1 ; col= col+1)
             {
-                cout<<col<<" ";
+                cout<<'*'<<" ";
             }
             cout<<endl;
         }
