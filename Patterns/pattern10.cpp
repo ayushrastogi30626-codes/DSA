bfe6f2555b8f3a0c85a09fe1f71f141fe050643a
Ayush Rastogi <YOUR_NOREPLY_EMAIL>
2026-07-13 11:55:20 +0530

    10th pattern problem

---
+++ Patterns/pattern10.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int row,col,n;
+    cout<<"Input the value: ";
+    cin>>n; 
+    for(row=1 ; row<=n; row= row+1){
+        for(col= 1 ; col<=n-(row-1) ; col=col+1)
+        {
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+}
