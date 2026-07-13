1c7f1348c9d11882457e6fde61e160535c6631c1
Ayush Rastogi <YOUR_NOREPLY_EMAIL>
2026-07-13 19:25:57 +0530

    11th pattern problem

---
+++ Patterns/pattern11.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int row,col,n;
+    cout<<"Input the value: ";
+    cin>>n;
+    for(row=1; row<=n ; row=row+1)
+    {
+        //Space
+        for(col=1 ; col<=n-row ; col= col+1)
+       {
+         cout<<" ";
+       }
+          //*
+        {
+        for(col=1; col<=row ; col= col+1)
+        { 
+            cout<<"*";
+         }
+    cout<<endl;
+        }
+}
+}
